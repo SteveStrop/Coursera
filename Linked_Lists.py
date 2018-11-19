f4b9732f8e517dd505084a96f3f857eb23c0fa5c
SteveStrop <steve_tropical@yahoo.co.uk>
2018-11-19 09:17:30 +0000

    refactored enqueue

--- Linked_Lists.py
+++ Linked_Lists.py
@@ -2,15 +2,7 @@ class Node:
 
     def __init__(self, data):
         self.data = data
-        self.link = None
-
-
-# head = None
-# for i in range(10):
-#     new_node = Node(i)
-#     new_node.link = head
-#     head = new_node
-#     print_nodes(head)
+        self.node = None
 
 
 class Stack:
@@ -24,23 +16,23 @@ class Stack:
 
     def push(self, data):
         new_node = Node(data)
-        new_node.link = self.head
+        new_node.node = self.head
         self.head = new_node
         self.n += 1
 
-    def __str__(self):
+    def __repr__(self):
         item = self.head
         count = 0
         result = ""
-        while item.link is not None:
-            result += f"Node {count}: {item.data,item.link}\n"
+        while item.data is not None:
+            result += f"Node {count}: {item.data,item.node}\n"
             count += 1
-            item = item.link
+            item = item.node
         return result
 
     def pop(self):
         result = self.head.data
-        self.head = self.head.link
+        self.head = self.head.node
         self.n -= 1
         return result
 
@@ -48,18 +40,16 @@ class Stack:
         return self.n == 0
 
 
-print("-" * 100 + "Stack" + "-" * 100)
-s = Stack()
+def test_stack():
+    print("-" * 100 + "Stack" + "-" * 100)
+    s = Stack()
+    for i in range(10):
+        s.push(i)
+    print(s)
+    while not s.is_empty():
+        print(s.pop())
 
-data = "first to be or not to be top"
-data = data.split()
-for word in data:
-    s.push(word)
-print(s)
-print(s.pop())
-print(s)
-
-
+test_stack()
 class Queue:
 
     def __init__(self):
@@ -70,29 +60,40 @@ class Queue:
     def __sizeof__(self):
         return self.n
 
+    def enqueue2(self, data):
+        body = self.last
+        self.last = Node(data)  # this is a new node
+
+        if self.is_empty():
+            self.first = self.last
+        else:
+            body.node = self.last  # this adds body.node to self.first.node recursively ???? How is body node and
+            # self.first.node linked?
+        self.n += 1
+
     def enqueue(self, data):
-        temp = self.last
-        self.last=Node(data)
+        new_node=Node(data)
+        self.last.node=new_node
+        self.last = new_node
         if self.is_empty():
             self.first=self.last
-        else:
-            temp.link=self.last
         self.n+=1
 
-
-    def __str__(self):
+    def __repr__(self):
         item = self.first
         count = 0
         result = ""
-        while item.link is not None:
-            result += f"Node {count}: {item.data,item.link}\n"
+        while True:
+            result += f"Node {count}: {item.data,item.node}\n"
             count += 1
-            item = item.link
+            if item.node is None:
+                break
+            item = item.node
         return result
 
     def dequeue(self):
         result = self.first.data
-        self.first = self.first.link
+        self.first = self.first.node
         self.n -= 1
         return result
 
@@ -103,7 +104,8 @@ class Queue:
 print("-" * 100 + "Queue" + "-" * 100)
 q = Queue()
 
-for i in range(100):
+for i in range(5):
     q.enqueue(i)
+print(q)
 while not q.is_empty():
     print(q.dequeue())
