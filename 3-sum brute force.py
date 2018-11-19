4d2b96ffadd2d377191773537fddefa081836ae9
SteveStrop <steve_tropical@yahoo.co.uk>
2018-11-19 15:03:53 +0000

    pop and dequeue made more robust by re initialising if queue / stack length drop to zero
    refactored 3-sum

--- 3-sum brute force.py
+++ 3-sum brute force.py
@@ -14,18 +14,6 @@ def three_sum(a):
     return count
 
 
-def three_sum_bin(a):
-    count = 0
-    a = sorted(a)
-    for i in range(len(a)):
-        for j in range(i + 1, len(a)):
-            if bin_search(a[j + 1:], -(a[i] + a[j])) != -1:
-                # print(f"a[{i}]:{a[i]} , a[{j}]:{a[j]}, {a[bin_search(a[j+1:], -(a[i] + a[j]))]}")
-                count += 1
-
-    return count
-
-
 def bin_search(a, key):
     lo, high = 0, len(a) - 1
     while lo <= high:
@@ -39,8 +27,23 @@ def bin_search(a, key):
     return -1
 
 
-a = [randint(-100000, 100000) for _ in range(1000)]
-a = sorted(list(set(a)))
-print(a)
-#print(three_sum(a))
-print(three_sum_bin(a))
+def three_sum_bin(a):
+    count = 0
+    a = sorted(a)
+    for i in range(len(a)):
+        for j in range(i + 1, len(a)):
+            if bin_search(a[j + 1:], -(a[i] + a[j])) != -1:
+                # print(f"a[{i}]:{a[i]} , a[{j}]:{a[j]}, {a[bin_search(a[j+1:], -(a[i] + a[j]))]}")
+                count += 1
+
+    return count
+
+
+def test_three_sum_bin():
+    a = [randint(-100000, 100000) for _ in range(1000)]
+    a = sorted(list(set(a)))
+    print(three_sum_bin(a))
+
+
+if __name__ == '__main__':
+    test_three_sum_bin()
--- Linked_Lists.py
+++ Linked_Lists.py
@@ -15,12 +15,9 @@ class Stack:
         return self.n
 
     def push(self, data):
-        if self.n:
-            new_node = Node(data)
-            new_node.node = self.head
-            self.head = new_node
-        else:
-            self.head.data = data
+        new_node = Node(data)
+        new_node.node = self.head
+        self.head = new_node
         self.n += 1
 
     def __repr__(self):
@@ -38,7 +35,7 @@ class Stack:
 
     def pop(self):
         if self.is_empty():
-            self.head = Node(None)
+            # self.head = Node(None)  # need this because self.head can be set to None if stack becomes empty
             return None
         data = self.head.data
         self.head = self.head.node
@@ -57,20 +54,6 @@ class Queue:
         self.first = Node(None)  # need to initialise all instance variables in the constructor (?)
         self.n = 0
 
-    def __sizeof__(self):
-        return self.n
-
-    def enqueue2(self, data):  # not used superseded by enqueue below
-        body = self.last
-        self.last = Node(data)  # this is a new node
-
-        if self.is_empty():
-            self.first = self.last
-        else:
-            body.node = self.last  # this adds body.node to self.first.node recursively ???? How is body node and
-            # self.first.node linked?
-        self.n += 1
-
     def enqueue(self, data):
         new_node = Node(data)  # create new last node for the new data
         self.last.node = new_node  # set the current self.last node pointer to the new node (previously this was None)
@@ -79,6 +62,20 @@ class Queue:
         if self.is_empty():
             self.first = self.last
         self.n += 1
+
+    def dequeue(self):
+        if self.is_empty():
+            return None
+        data = self.first.data
+        self.first = self.first.node
+        self.n -= 1
+        return data
+
+    def is_empty(self):
+        return self.n == 0
+
+    def __sizeof__(self):
+        return self.n
 
     def __repr__(self):
         item = self.first
@@ -93,16 +90,16 @@ class Queue:
 
         return display_string
 
-    def dequeue(self):
-        if self.is_empty():
-            return None
-        data = self.first.data
-        self.first = self.first.node
-        self.n -= 1
-        return data
-
-    def is_empty(self):
-        return self.n == 0
+    # def __enqueue__(self, data):  # not used superseded by enqueue below
+    #     body = self.last
+    #     self.last = Node(data)  # this is a new node
+    #
+    #     if self.is_empty():
+    #         self.first = self.last
+    #     else:
+    #         body.node = self.last  # this adds body.node to self.first.node recursively ???? How is body node and
+    #         # self.first.node linked?
+    #     self.n += 1
 
 
 def test_stack():
