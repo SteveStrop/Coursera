b58a9e604eb05774fe060c253b6fd9dc11eef159
SteveStrop <steve_tropical@yahoo.co.uk>
2018-11-22 20:01:15 +0000

    refactored main method into a class (TM)

--- DFA.py
+++ DFA.py
@@ -8,12 +8,12 @@ False 2 0
 
 first line is number of states
 second line is alphabet
-subsequent lines are result and actions for each state/letter combination
+subsequent lines are result and actions for each initial_state/letter combination
 
 """
 
-# parse the input data to produce a DFA alphabet, a list of True/False actions for each state and a dictionary of
-# operations for each state
+# parse the input data to produce a DFA alphabet, a list of True/False actions for each initial_state and a dictionary of
+# operations for each initial_state
 with open("DFA.txt", "r")as f:
     stages = int(f.readline())
     alphabet = f.readline().rstrip("\n")
@@ -30,7 +30,7 @@ print(action[state])
 
 print(f"Stages: {state_count}")
 print(f"Alphabet: {alphabet}")
-print(f"Actions: {action}")
+print(f"Actions: {actions}")
 print(f"Transitions: {next_state}")
 
 
--- TM.py
+++ TM.py
@@ -1,44 +1,82 @@
-"""
-Sample data file (binary add):\n
-6  2                     (number of states N , start state)\n
-10+#                    (alphabet)\n
-L 1 0 4 0 0 1 # #       (state N transitions then N outputs per letter)\n
-L 1 1 3 1 1 0 + #\n
-R 2 2 2 0 1 0 + #\n
-L 3 2 3 2 0 1 + 1\n
-R 4 4 4 5 # 0 + #\n
-H 5 5 5 5 1 0 + #\n
-
-"""
-
 from Linked_Lists import Stack
 
 
-action = []
-transition = []
-output = []
+class TM:
 
-with open("TM.txt", "r")as f:
-    state_count, state = map(int, f.readline().split())
-    alphabet = f.readline().rstrip("\n")
-    alpha_count = len(alphabet)
-    for _ in range(state_count):
-        data = f.readline().rstrip("\n").split()
-        action.append(data[0])
-        t = {alphabet[i]: int(data[i + 1]) for i in range(alpha_count)}
-        o = {alphabet[i]: data[i + alpha_count + 1] for i in range(alpha_count)}  # alpha_count is offset to output
-        output.append(o)
-
-
-def print_machine_defn():
-    print(f"Stages: {state_count}")
-    print(f"Alphabet: {alphabet}")
-    print(f"Actions: {action}")
-    print(f"Outputs: {output}")
-    print(f"Transitions: {transition}")
+    def __init__(self):
+        self.tape = Tape()
+        # initialised in mount
+        self.calc = None
+        # initialised in load_machine
+        self.state_count = None
+        self.alphabet = None
+        self.actions = None
+        self.transitions = None
+        self.outputs = None
+        self.initial_state = None
 
 
-# print_machine_defn()
+    def mount(self, calc):
+        """Mount a tape into the turing machine"""
+        self.calc = calc
+        self.tape.load(self.calc)
+
+    def load_machine(self, file):
+        """ Read a turing machine specification from a file.
+
+        Sample data file (binary add):
+        6  2                     (number of states N , start initial_state)\n
+        10+#                    (alphabet)\n
+        L 1 0 4 0 0 1 # #       (initial_state N transitions then N outputs per letter)\n
+        L 1 1 3 1 1 0 + #\n
+        R 2 2 2 0 1 0 + #\n
+        L 3 2 3 2 0 1 + 1\n
+        R 4 4 4 5 # 0 + #\n
+        H 5 5 5 5 1 0 + #\n
+
+        """
+        self.actions = []  # list of all state actions
+        self.transitions = []  # list of transitions from one state to another
+        self.outputs = []  # list of outputs corresponding to each transitions
+
+        with open(file, "r")as f:
+            self.state_count, self.initial_state = map(int, f.readline().split())
+            self.alphabet = f.readline().rstrip("\n")
+            alpha_count = len(self.alphabet)
+            for _ in range(self.state_count):
+                data = f.readline().rstrip("\n").split()
+                # save state type (L)eft, (R)ight, (H)alt
+                self.actions.append(data[0])
+                # create dict of transitions for this state (transitions start at position 1 in data list)
+                t = {self.alphabet[i]: int(data[i + 1]) for i in range(alpha_count)}
+                # create dict of outputs for this state (outputs start at position alpha_count +1 in data list)
+                o = {self.alphabet[i]: data[i + alpha_count + 1] for i in range(alpha_count)}
+                # add this states transitions and outputs to the master lists
+                self.transitions.append(t)
+                self.outputs.append(o)
+
+    def print_machine_defn(self):
+        """ Print out specification of loaded machine."""
+        print(f"States: {self.state_count}")
+        print(f"Alphabet: {self.alphabet}")
+        print(f"Actions: {self.actions}")
+        print(f"Outputs: {self.outputs}")
+        print(f"Transitions: {self.transitions}")
+
+    def run(self):
+        state = self.initial_state
+        while self.actions[state] != "H":
+            inpt = self.tape.read()
+            self.tape.write(self.outputs[state][inpt])
+            state = self.transitions[state][inpt]
+            if self.actions[state] == "L":
+                self.tape.move_left()
+            elif self.actions[state] == "R":
+                self.tape.move_right()
+        self.print_result()
+
+    def print_result(self):
+        print(self.calc, "=", self.tape.read_tape())
 
 
 class Tape:
@@ -76,19 +114,7 @@ class Tape:
         return result.strip("#")
 
 
-test_string = "1110+1"
-numbers = test_string.split("+")
-
-t = Tape()
-t.load(test_string)
-while action[state] != "H":
-    inpt = t.read()
-    t.write(output[state][inpt])
-    state = transition[state][inpt]
-    if action[state] == "L":
-        t.move_left()
-    elif action[state] == "R":
-        t.move_right()
-answer = t.read_tape()
-print(int(numbers[0], 2), "+", int(numbers[1], 2), "=", int(answer, 2))
-print(f"{test_string} = {answer}")
+tm = TM()
+tm.load_machine("binary_add.txt")
+tm.mount("1110+1")
+tm.run()
